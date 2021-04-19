338793f0466831e67eb90560e6703470f4c763dd
Dorota Tomala <doforever@gmail.com>
2021-04-19 13:01:02 +0200

    Create concerts routes

---
+++ routes/concerts.routes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db');
+
+// { id: 1, performer: 'John Doe', genre: 'Rock', price: 25, day: 1, image: '/img/uploads/1fsd324fsdg.jpg' },
+
+router.route('/concerts').get((req, res) => {
+  res.json(db.concerts);
+});
+
+router.route('/concerts/:id').get((req, res) => {
+  const item = db.concerts.find(item => item.id == req.params.id);
+  if (item) res.json(item);
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+router.route('/concerts').post((req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+
+  if (performer && genre && price && day && image) {
+    const id = uuidv4();
+    db.concerts.push({ id, performer, genre, price, day, image });
+    res.json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+router.route('/concerts/:id').put((req, res) => {
+  const item = db.concerts.find(item => item.id == req.params.id);
+  const { performer, genre, price, day, image } = req.body;
+
+  if (item && performer && genre && price && day && image) {
+    Object.assign(item, { performer, genre, price, day, image });
+    res.json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+router.route('/concerts/:id').delete((req, res) => {
+  const item = db.concerts.find(item => item.id == req.params.id);
+  if (item) {
+    db.concerts.splice(db.concerts.indexOf(item), 1);
+    res.json({ message: 'OK' });
+  }
+  else res.status(404).json({ message: 'Not found...' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const cors = require('cors');
 const app = express();
 
 // import routes
-const testimonialRoutes = require('./routes/testimonials.routes');
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+// const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/api', testimonialRoutes);
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+// app.use('/api', seatsRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
