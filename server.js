892fdc00cc5791b9e2ace95b66c3994b34b2fec7
Dorota Tomala <doforever@gmail.com>
2021-04-20 11:21:31 +0200

    Modify console log in server

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use((req, res) => {
 });
 
 app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+  console.log('Server is running on port: ', process.env.PORT || 8000);
 });
