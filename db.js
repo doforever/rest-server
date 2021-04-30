218301cbd35350dae94c2a6ef399ecbc0a3cca1f
Dorota Tomala <doforever@gmail.com>
2021-04-30 09:23:51 +0200

    Prepare collection json files and models

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ const db = {
   ],
   concerts: [
     { id: '1', performer: 'John Doe', genre: 'Rock', price: 25, day: 1, image: '/img/uploads/1fsd324fsdg.jpg' },
-    { id: '2', performer: 'Rebekah Parker', genre: 'R&B', price: 25, day: 1, image: '/img/uploads/2f342s4fsdg.jpg' },
-    { id: '3', performer: 'Maybell Haley', genre: 'Pop', price: 40, day: 1, image: '/img/uploads/hdfh42sd213.jpg' },
+    { id: '2', performer: 'Rebekah Parker', genre: 'R&B', price: 25, day: 2, image: '/img/uploads/2f342s4fsdg.jpg' },
+    { id: '3', performer: 'Maybell Haley', genre: 'Pop', price: 40, day: 3, image: '/img/uploads/hdfh42sd213.jpg' },
   ],
   seats: [
     { id: '1', day: 1, seat: 3, client: 'Amanda Doe', email: 'amandadoe@example.com' },
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -33,6 +34,15 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
+
+// connects our backend code with the database
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: ', process.env.PORT || 8000);
